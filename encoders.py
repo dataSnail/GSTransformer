984b0436caed36607da975c18f72a742b84036f7
Qing Meng <qingmeng@Big-MacBook-Pro.local>
2021-04-18 23:12:12 +0800

    gstransformerv0.1 original transformer

--- encoders.py
+++ encoders.py
@@ -6,17 +6,176 @@
 '''
 
 import torch
+import math
 import torch.nn as nn
 from torch.nn import init
 import torch.nn.functional as F
 
 import numpy as np
 
-class GSTransformer(nn.Module):
-    def __init__(self, input_dim):
-        self.transformer = nn.Transformer(d_model=512, nhead=8, num_encoder_layers=6, num_decoder_layers=6,
-                                          dim_feedforward=2048, dropout=0.1, activation='relu',
-                                          custom_encoder=None, custom_decoder=None)
+# Temporarily leave PositionalEncoding module here. Will be moved somewhere else.
+class PositionalEncoding(nn.Module):
+    r"""Inject some information about the relative or absolute position of the tokens
+        in the sequence. The positional encodings have the same dimension as
+        the embeddings, so that the two can be summed. Here, we use sine and cosine
+        functions of different frequencies.
+    .. math::
+        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
+        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
+        \text{where pos is the word position and i is the embed idx)
+    Args:
+        d_model: the embed dim (required).
+        dropout: the dropout value (default=0.1).
+        max_len: the max. length of the incoming sequence (default=5000).
+    Examples:
+        >>> pos_encoder = PositionalEncoding(d_model)
+    """
 
-    def forward(self):
-        pass
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        r"""Inputs of forward function
+        Args:
+            x: the sequence fed to the positional encoder model (required).
+        Shape:
+            x: [sequence length, batch size, embed dim]
+            output: [sequence length, batch size, embed dim]
+        Examples:
+            >>> output = pos_encoder(x)
+        """
+
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+
+class GSTransformer(nn.Module):
+    def __init__(self, feats, nhead, nhid, nlayers, nclass, dropout=0.5, fine_tune=False):
+        """
+        :param feats:  predefined feature matrix
+        :param ninp:   the dimension of input
+        :param nhead:  the head num
+        :param nhid:   the hidden dimension of encoder layer
+        :param nlayers:    the number of encoder layer
+        :param dropout:
+        :param fine_tune:   fine-tune the predefined feature matrix
+        """
+        super(GSTransformer, self).__init__()
+        try:
+            from torch.nn import TransformerEncoder, TransformerEncoderLayer
+        except:
+            raise ImportError('TransformerEncoder module does not exist in PyTorch 1.1 or lower.')
+        self.model_type = 'Transformer'
+        self.ninp = feats.size(1)  # 输入维度
+        self.seq_mask = None
+        self.pos_encoder = PositionalEncoding(self.ninp, dropout)  # 位置编码层 ninp is the dimension of input
+        encoder_layers = TransformerEncoderLayer(self.ninp, nhead, nhid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+
+        # emebedding  layer using predefined feats
+        # https://discuss.pytorch.org/t/can-we-use-pre-trained-word-embeddings-for-weight-initialization-in-nn-embedding/1222/2
+        self.embedding = nn.Embedding(feats.size(0), feats.size(1))
+        self.embedding.weight = nn.Parameter(feats)
+        self.embedding.weight.requires_grad = fine_tune
+
+        self.pred = self.build_pred_layers(self.ninp, 0, nclass)
+
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.1
+        # nn.init.uniform_(self.encoder.weight, -initrange, initrange)
+        # nn.init.zeros_(self.decoder.weight)
+        # nn.init.uniform_(self.decoder.weight, -initrange, initrange)
+
+    def _generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+    def build_pred_layers(self, pred_input_dim, pred_hidden_dims, label_dim, num_aggs=1):
+        """
+        transformer后的预测层
+        """
+        pred_input_dim = pred_input_dim * num_aggs
+        if len(pred_hidden_dims) == 0:
+            pred_model = nn.Linear(pred_input_dim, label_dim)
+        else:
+            pred_layers = []
+            for pred_dim in pred_hidden_dims:
+                pred_layers.append(nn.Linear(pred_input_dim, pred_dim))
+                pred_layers.append(self.act)
+                pred_input_dim = pred_dim
+            pred_layers.append(nn.Linear(pred_dim, label_dim))
+            pred_model = nn.Sequential(*pred_layers)
+        return pred_model
+
+    def forward(self, seq, has_mask=False):
+        cls = torch.ones(seq.size(0), self.ninp)  #
+        emb = self.embedding(seq)  # ntoken x ninp
+        torch.cat([cls, emb], dim=1)
+
+        if has_mask:
+            device = emb.device
+            if self.seq_mask is None or self.seq_mask.size(0) != len(seq):
+                mask = self._generate_square_subsequent_mask(len(seq)).to(device)
+                self.seq_mask = mask
+        else:
+            self.src_mask = None
+
+        emb = self.pos_encoder(emb)
+        output = self.transformer_encoder(emb)
+        output = self.pred(output)
+
+        return output
+
+    def loss(self, pred, label):
+        '''
+        Args:
+            batch_num_nodes: numpy array of number of nodes in each graph in the minibatch.
+        '''
+        eps = 1e-7
+
+        if type == 'softmax':
+            loss = F.cross_entropy(pred, label, reduction='mean')
+        elif type == 'margin':
+            batch_size = pred.size()[0]
+            label_onehot = torch.zeros(batch_size, self.label_dim).long().cuda()
+            label_onehot.scatter_(1, label.view(-1,1), 1)
+            loss = torch.nn.MultiLabelMarginLoss()(pred, label_onehot)
+
+        # if self.linkpred:
+        #     max_num_nodes = adj.size()[1]
+        #     pred_adj0 = self.assign_tensor @ torch.transpose(self.assign_tensor, 1, 2)
+        #     tmp = pred_adj0
+        #     pred_adj = pred_adj0
+        #     for adj_pow in range(adj_hop-1):  #这是什么意思？
+        #         tmp = tmp @ pred_adj0
+        #         pred_adj = pred_adj + tmp
+        #     pred_adj = torch.min(pred_adj, torch.ones(1, dtype=pred_adj.dtype).cuda())  # 大于1取1
+        #     #print('adj1', torch.sum(pred_adj0) / torch.numel(pred_adj0))
+        #     #print('adj2', torch.sum(pred_adj) / torch.numel(pred_adj))
+        #     #self.link_loss = F.nll_loss(torch.log(pred_adj), adj)
+        #     self.link_loss = -adj * torch.log(pred_adj+eps) - (1-adj) * torch.log(1-pred_adj+eps)
+        #     if batch_num_nodes is None:
+        #         num_entries = max_num_nodes * max_num_nodes * adj.size()[0]
+        #         print('Warning: calculating link pred loss without masking')
+        #     else:
+        #         num_entries = np.sum(batch_num_nodes * batch_num_nodes)
+        #         embedding_mask = self.construct_mask(max_num_nodes, batch_num_nodes)
+        #         adj_mask = embedding_mask @ torch.transpose(embedding_mask, 1, 2)
+        #         self.link_loss[(1-adj_mask).bool()] = 0.0
+        #
+        #     self.link_loss = torch.sum(self.link_loss) / float(num_entries)
+        #     #print('linkloss: ', self.link_loss)
+        #     return loss + self.link_loss
+        return loss
--- train.py
+++ train.py
@@ -52,121 +52,121 @@ def benchmark_task_val(args, writer=None, feat='node-label'):
                     bn=args.bn, dropout=args.dropout, linkpred=args.linkpred, args=args,
                     assign_input_dim=assign_input_dim).cuda()
 
-    #     _, val_accs = train(train_dataset, model, args, val_dataset=val_dataset, test_dataset=None,
-    #         writer=writer)
-    #     all_vals.append(np.array(val_accs))
-    # all_vals = np.vstack(all_vals)
-    # all_vals = np.mean(all_vals, axis=0)
-    # print(all_vals)
-    # print(np.max(all_vals))
-    # print(np.argmax(all_vals))
+        _, val_accs = train(train_dataset, model, args, val_dataset=val_dataset, test_dataset=None,
+            writer=writer)
+        all_vals.append(np.array(val_accs))
+    all_vals = np.vstack(all_vals)
+    all_vals = np.mean(all_vals, axis=0)
+    print(all_vals)
+    print(np.max(all_vals))
+    print(np.argmax(all_vals))
 
-#
-# def train(dataset, model, args, same_feat=True, val_dataset=None, test_dataset=None, writer=None,
-#           mask_nodes=True):
-#     writer_batch_idx = [0, 3, 6, 9]
-#
-#     optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001)
-#     iter = 0
-#     best_val_result = {
-#         'epoch': 0,
-#         'loss': 0,
-#         'acc': 0}
-#     test_result = {
-#         'epoch': 0,
-#         'loss': 0,
-#         'acc': 0}
-#     train_accs = []
-#     train_epochs = []
-#     best_val_accs = []
-#     best_val_epochs = []
-#     test_accs = []
-#     test_epochs = []
-#     val_accs = []
-#     for epoch in range(args.num_epochs):
-#         total_time = 0
-#         avg_loss = 0.0
-#         model.train()
-#         print('Epoch: ', epoch)
-#         for batch_idx, data in enumerate(dataset):
-#             begin_time = time.time()
-#             model.zero_grad()
-#             adj = Variable(data['adj'].float(), requires_grad=False).cuda()
-#             h0 = Variable(data['feats'].float(), requires_grad=False).cuda()
-#             label = Variable(data['label'].long()).cuda()
-#             batch_num_nodes = data['num_nodes'].int().numpy() if mask_nodes else None
-#             assign_input = Variable(data['assign_feats'].float(), requires_grad=False).cuda()
-#
-#             ypred = model(h0, adj, batch_num_nodes, assign_x=assign_input)
-#             if not args.method == 'soft-assign' or not args.linkpred:
-#                 loss = model.loss(ypred, label)
-#             else:
-#                 loss = model.loss(ypred, label, adj, batch_num_nodes)
-#             loss.backward()
-#             nn.utils.clip_grad_norm_(model.parameters(), args.clip)
-#             optimizer.step()
-#             iter += 1
-#             avg_loss += loss
-#             # if iter % 20 == 0:
-#             #    print('Iter: ', iter, ', loss: ', loss.data[0])
-#             elapsed = time.time() - begin_time
-#             total_time += elapsed
-#
-#             # log once per XX epochs
-#             if epoch % 10 == 0 and batch_idx == len(
-#                     dataset) // 2 and args.method == 'soft-assign' and writer is not None:
-#                 log_assignment(model.assign_tensor, writer, epoch, writer_batch_idx)
-#                 if args.log_graph:
-#                     log_graph(adj, batch_num_nodes, writer, epoch, writer_batch_idx, model.assign_tensor)
-#         avg_loss /= batch_idx + 1
-#         if writer is not None:
-#             writer.add_scalar('loss/avg_loss', avg_loss, epoch)
-#             if args.linkpred:
-#                 writer.add_scalar('loss/linkpred_loss', model.link_loss, epoch)
-#         print('Avg loss: ', avg_loss, '; epoch time: ', total_time)
-#         result = evaluate(dataset, model, args, name='Train', max_num_examples=100)
-#         train_accs.append(result['acc'])
-#         train_epochs.append(epoch)
-#         if val_dataset is not None:
-#             val_result = evaluate(val_dataset, model, args, name='Validation')
-#             val_accs.append(val_result['acc'])
-#         if val_result['acc'] > best_val_result['acc'] - 1e-7:
-#             best_val_result['acc'] = val_result['acc']
-#             best_val_result['epoch'] = epoch
-#             best_val_result['loss'] = avg_loss
-#         if test_dataset is not None:
-#             test_result = evaluate(test_dataset, model, args, name='Test')
-#             test_result['epoch'] = epoch
-#         if writer is not None:
-#             writer.add_scalar('acc/train_acc', result['acc'], epoch)
-#             writer.add_scalar('acc/val_acc', val_result['acc'], epoch)
-#             writer.add_scalar('loss/best_val_loss', best_val_result['loss'], epoch)
-#             if test_dataset is not None:
-#                 writer.add_scalar('acc/test_acc', test_result['acc'], epoch)
-#
-#         print('Best val result: ', best_val_result)
-#         best_val_epochs.append(best_val_result['epoch'])
-#         best_val_accs.append(best_val_result['acc'])
-#         if test_dataset is not None:
-#             print('Test result: ', test_result)
-#             test_epochs.append(test_result['epoch'])
-#             test_accs.append(test_result['acc'])
-#
-#     matplotlib.style.use('seaborn')
-#     plt.switch_backend('agg')
-#     plt.figure()
-#     plt.plot(train_epochs, util.exp_moving_avg(train_accs, 0.85), '-', lw=1)
-#     if test_dataset is not None:
-#         plt.plot(best_val_epochs, best_val_accs, 'bo', test_epochs, test_accs, 'go')
-#         plt.legend(['train', 'val', 'test'])
-#     else:
-#         plt.plot(best_val_epochs, best_val_accs, 'bo')
-#         plt.legend(['train', 'val'])
-#     plt.savefig(gen_train_plt_name(args), dpi=600)
-#     plt.close()
-#     matplotlib.style.use('default')
-#
-#     return model, val_accs
+
+def train(dataset, model, args, same_feat=True, val_dataset=None, test_dataset=None, writer=None,
+          mask_nodes=True):
+    writer_batch_idx = [0, 3, 6, 9]
+
+    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001)
+    iter = 0
+    best_val_result = {
+        'epoch': 0,
+        'loss': 0,
+        'acc': 0}
+    test_result = {
+        'epoch': 0,
+        'loss': 0,
+        'acc': 0}
+    train_accs = []
+    train_epochs = []
+    best_val_accs = []
+    best_val_epochs = []
+    test_accs = []
+    test_epochs = []
+    val_accs = []
+    for epoch in range(args.num_epochs):
+        total_time = 0
+        avg_loss = 0.0
+        model.train()
+        print('Epoch: ', epoch)
+        for batch_idx, data in enumerate(dataset):
+            begin_time = time.time()
+            model.zero_grad()
+            seq = Variable(data['sequence'].float(), requires_grad=False).cuda()
+            h0 = Variable(data['feats'].float(), requires_grad=False).cuda()
+            label = Variable(data['label'].long()).cuda()
+            batch_num_nodes = data['num_nodes'].int().numpy() if mask_nodes else None
+
+            ypred = model(h0, 1, 64, 4, assign_x=assign_input)
+
+            if not args.method == 'soft-assign' or not args.linkpred:
+                loss = model.loss(ypred, label)
+            else:
+                loss = model.loss(ypred, label, adj, batch_num_nodes)
+            loss.backward()
+            nn.utils.clip_grad_norm_(model.parameters(), args.clip)  # 梯度裁剪
+            optimizer.step()
+            iter += 1
+            avg_loss += loss
+            # if iter % 20 == 0:
+            #    print('Iter: ', iter, ', loss: ', loss.data[0])
+            elapsed = time.time() - begin_time
+            total_time += elapsed
+
+            # log once per XX epochs
+            if epoch % 10 == 0 and batch_idx == len(
+                    dataset) // 2 and args.method == 'soft-assign' and writer is not None:
+                log_assignment(model.assign_tensor, writer, epoch, writer_batch_idx)
+                if args.log_graph:
+                    log_graph(adj, batch_num_nodes, writer, epoch, writer_batch_idx, model.assign_tensor)
+        avg_loss /= batch_idx + 1
+        if writer is not None:
+            writer.add_scalar('loss/avg_loss', avg_loss, epoch)
+            if args.linkpred:
+                writer.add_scalar('loss/linkpred_loss', model.link_loss, epoch)
+        print('Avg loss: ', avg_loss, '; epoch time: ', total_time)
+        result = evaluate(dataset, model, args, name='Train', max_num_examples=100)
+        train_accs.append(result['acc'])
+        train_epochs.append(epoch)
+        if val_dataset is not None:
+            val_result = evaluate(val_dataset, model, args, name='Validation')
+            val_accs.append(val_result['acc'])
+        if val_result['acc'] > best_val_result['acc'] - 1e-7:
+            best_val_result['acc'] = val_result['acc']
+            best_val_result['epoch'] = epoch
+            best_val_result['loss'] = avg_loss
+        if test_dataset is not None:
+            test_result = evaluate(test_dataset, model, args, name='Test')
+            test_result['epoch'] = epoch
+        if writer is not None:
+            writer.add_scalar('acc/train_acc', result['acc'], epoch)
+            writer.add_scalar('acc/val_acc', val_result['acc'], epoch)
+            writer.add_scalar('loss/best_val_loss', best_val_result['loss'], epoch)
+            if test_dataset is not None:
+                writer.add_scalar('acc/test_acc', test_result['acc'], epoch)
+
+        print('Best val result: ', best_val_result)
+        best_val_epochs.append(best_val_result['epoch'])
+        best_val_accs.append(best_val_result['acc'])
+        if test_dataset is not None:
+            print('Test result: ', test_result)
+            test_epochs.append(test_result['epoch'])
+            test_accs.append(test_result['acc'])
+
+    matplotlib.style.use('seaborn')
+    plt.switch_backend('agg')
+    plt.figure()
+    plt.plot(train_epochs, util.exp_moving_avg(train_accs, 0.85), '-', lw=1)
+    if test_dataset is not None:
+        plt.plot(best_val_epochs, best_val_accs, 'bo', test_epochs, test_accs, 'go')
+        plt.legend(['train', 'val', 'test'])
+    else:
+        plt.plot(best_val_epochs, best_val_accs, 'bo')
+        plt.legend(['train', 'val'])
+    plt.savefig(gen_train_plt_name(args), dpi=600)
+    plt.close()
+    matplotlib.style.use('default')
+
+    return model, val_accs
 
 
 def gen_prefix(args):
@@ -296,15 +296,12 @@ def main():
         print('Remove existing log dir: ', path)
         shutil.rmtree(path)
     writer = SummaryWriter(path)
-    #writer = None
 
     os.environ['CUDA_VISIBLE_DEVICES'] = prog_args.cuda
     print('CUDA', prog_args.cuda)
 
-
     if prog_args.bmname is not None:
         benchmark_task_val(prog_args, writer=writer)
-
 
     writer.close()
 
